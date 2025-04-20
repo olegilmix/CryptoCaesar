ce9689f214ab59ef2bf980aafc9e158dd839fd3f
olegilmix <olegilmix@yandex.ru>
2025-04-20 07:52:29 +0300

    Initial commit

---
+++ lib.py
@@ -0,0 +1,71 @@
+from string import punctuation
+
+lang_en = [chr(i) for i in range(65,91)] + [chr(j) for j in range(97,123)]
+lang_ru = [chr(i) for i in range(1040,1104)]
+
+def is_valid_number(message, start, end):
+    while True:
+        try:
+            print(message)
+            result = int(input())
+            if start <= result <= end:
+                return result
+            else:
+                print(f'Ошибка число должно быть от {start} до {end}!')
+        except ValueError:
+            print('Ошибка: Введите число')
+
+def is_valid_text(lang):
+    while True:
+        print('Введите текст')
+        txt = input()
+        if txt == '':
+            print('Ошибка: Пустой текст')
+            continue
+        elif txt[0] in punctuation:
+            print('Ошибка: Текст должен начинаться с буквы, а не с спец. символа')
+        elif lang == 1 and txt[0] in lang_en:
+            return txt
+        elif lang == 2 and txt[0] in lang_ru:
+            return txt
+        else:
+            print('Ошибка: Текст не соответствует выбранному языку')
+
+def code(lang, rotate, text):
+    result = ''
+    if lang == 1:
+        alphabet = lang_en
+        power = 26
+    else:
+        alphabet = lang_ru
+        power = 32
+
+    for symbol in text:
+        if symbol.isalpha():
+            if symbol.isupper():
+                result += alphabet[(alphabet.index(symbol) + rotate) % power]
+            else:
+                result += alphabet[(alphabet.index(symbol) + rotate) % power + power]
+        else:
+            result += symbol
+    
+    return result
+
+
+def decode(lang, rotate, text):
+    result = ''
+    if lang == 1:
+        alphabet = lang_en
+    else:
+        alphabet = lang_ru
+
+    for symbol in text:
+        if symbol.isalpha():
+            if symbol.isupper():
+                result += alphabet[(alphabet.index(symbol) - rotate) % 26]
+            else:
+                result += alphabet[(alphabet.index(symbol) - rotate) % 26 + 26]
+        else:
+            result += symbol
+    
+    return result
---
+++ run.py
@@ -0,0 +1,44 @@
+from time import sleep
+import lib
+import sys
+
+def caesar_crypto():
+    print('Шифр Цезаря v1.0. Author - olegilmix')
+    sleep(1)
+    print('Добро пожаловать!')
+    sleep(2)
+
+    caesar = lib.is_valid_number('1 - Старт\n2 - Выход', 1, 2)
+
+    if caesar == 2:
+        print('Всего хорошего!')
+        sys.exit(0)
+
+
+    print('Выберите тип направления:')
+    direction = lib.is_valid_number('1 - Шифрование\n2 - Дешифрование', 1, 2)
+
+    if direction == 3:
+        exit()
+
+    sleep(0.5)
+    print('Выберите язык')
+    lang = lib.is_valid_number('1 - Английский язык\n2 - Русский язык', 1, 2)
+
+    sleep(0.5)
+    if lang == 1:
+        rotate = lib.is_valid_number('Введите шаг сдвига (от 0 до 25)', 0, 25)
+    else:
+        rotate = lib.is_valid_number('Введите шаг сдвига (от 0 до 31)', 0, 31)
+
+    if direction == 1:
+        text = lib.is_valid_text(lang)
+        print('Зашифрованный текст:')
+        print(lib.code(lang, rotate, text))
+    else:
+        text = lib.is_valid_text(lang)
+        print('Расшифрованный текст:')
+        print(lib.decode(lang, rotate, text))
+
+if __name__ == "__main__":
+    caesar_crypto()
